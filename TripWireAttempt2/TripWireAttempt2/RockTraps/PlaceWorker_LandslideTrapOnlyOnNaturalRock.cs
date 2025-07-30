20953f18d4d0858fdd72718d4f048ffc91dc59e6
Miblech <3856768@alu.murciaeduca.es>
2025-07-30 16:41:36 +0200

    Sliding Trap Complete

---
+++ TripWireAttempt2/TripWireAttempt2/RockTraps/PlaceWorker_LandslideTrapOnlyOnNaturalRock.cs
@@ -0,0 +1,27 @@
+﻿using Verse;
+
+namespace TrapsExpanded
+{
+    public class PlaceWorker_LandslideTrapOnlyOnNaturalRock : PlaceWorker
+    {
+        public override AcceptanceReport AllowsPlacing(BuildableDef def, IntVec3 loc, Rot4 rot, Map map, Thing thingToIgnore = null, Thing thing = null)
+        {
+            IntVec3 wallCell = TrapUtils.DetermineWallCell(loc, rot);
+
+            if (!wallCell.InBounds(map))
+            {
+                return "Must be placed against a natural rock wall."; 
+            }
+
+            foreach (Thing foundThing in wallCell.GetThingList(map))
+            {
+                if (foundThing.def.building != null && foundThing.def.building.isNaturalRock)
+                {
+                    return true;
+                }
+            }
+
+            return "Must be placed against a natural rock wall.";
+        }
+    }
+}
---
+++ TripWireAttempt2/TripWireAttempt2/RockTraps/TrapUtils.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace TrapsExpanded
+{
+
+    public static class TrapUtils
+    {
+        public static IntVec3 DetermineWallCell(IntVec3 originCell, Rot4 rotation)
+    {
+        if (rotation == Rot4.North) // If object faces North, the wall is to its South
+        {
+            return originCell + IntVec3.South;
+        }
+        else if (rotation == Rot4.East) // If object faces East, the wall is to its West
+        {
+            return originCell + IntVec3.West;
+        }
+        else if (rotation == Rot4.South) // If object faces South, the wall is to its North
+        {
+            return originCell + IntVec3.North;
+        }
+        else // rotation == Rot4.West (If object faces West, the wall is to its East)
+        {
+            return originCell + IntVec3.East;
+        }
+    }
+}
+}
